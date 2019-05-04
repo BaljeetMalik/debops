80e3e17d3930f78c137b4aa1ea241c7fbf2e0904
Baljeet <baljeet1.singh@airtel.com>
2019-05-04 19:44:34 +0530

    find by name

--- src/main/java/com/test/rest/controller/UserController.java
+++ src/main/java/com/test/rest/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class UserController {
 	@GetMapping("/all")
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+
+	@GetMapping("/all/{name}")
+	public User findAll(@PathVariable String name) {
+		return userRepository.findAll().stream().filter(u -> u.getFirstName().contains(name)).findFirst().get();
 	}
 
 }
